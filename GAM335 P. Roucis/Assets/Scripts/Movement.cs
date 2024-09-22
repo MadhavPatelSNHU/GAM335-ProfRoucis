49cba68d5c6ea5592ebe2a3fb92d59189071e4a7
MadhavPatel <madhav.patel@snhu.edu>
2024-09-21 22:14:39 -0400

    simple scene and movement system not working

--- GAM335 P. Roucis/Assets/Scripts/Movement.cs
+++ GAM335 P. Roucis/Assets/Scripts/Movement.cs
@@ -8,13 +8,21 @@ public class Movement : MonoBehaviour
 {
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction jumpAction;
+    public Vector2 cam;
     [SerializeField] float speed = 10;
+    [SerializeField] float jumpPower = 10;
+    [SerializeField] float sensitivity;
+    float xRotation;
+    float yRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+        jumpAction = playerInput.actions.FindAction("Jump");
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
@@ -22,11 +30,27 @@ public class Movement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        CameraMove();
     }
 
     void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        transform.position += new Vector3(direction.x, direction.y) * speed * Time.deltaTime;
+
+        Vector3 jump = jumpAction.ReadValue<Vector3>(); 
+        transform.position += new Vector3(0, jump.z, 0) * jumpPower * Time.deltaTime;
+
+    }
+
+    void CameraMove()
+    {
+        xRotation -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity;
+        yRotation += Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity;
+        
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // to stop the player from looking above/below
+        
+        transform.localEulerAngles = new Vector3(xRotation, yRotation, 0);
     }
 }
+
