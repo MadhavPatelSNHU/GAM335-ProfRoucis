2c820b072f92c0ca25c39f39245a30e6f287f618
MadhavPatel <madhav.patel@snhu.edu>
2024-11-07 15:35:33 -0500

    classwork

---
+++ GAM335 P. Roucis/Assets/Scripts/Class/MyInventory.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyInventory : MonoBehaviour
+{
+    public MyItemDefinition[] Items;
+    public int capacity;
+
+    void Start()
+    {
+        Items = new MyItemDefinition[capacity];
+    }
+
+
+    public bool Add(MyItemDefinition item)
+    {
+        int? NextOpenIndex()
+        {
+
+            for (int idx = 0; idx < capacity; idx++)
+            {
+                if (Items[idx] == null)
+                {
+                    return idx;
+                }
+            }
+            return null;
+        }
+        int? nextIndexOrNull = NextOpenIndex();
+        if (nextIndexOrNull is int nextIndex)
+        {
+            Items[nextIndex] = item;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
